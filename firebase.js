3a9bdf2461d43b779ef21bef830311b569989abb
Ashwin <ash@mylazyfrog.com>
2021-07-28 16:32:17 -0400

    Remove firebase credentials

--- firebase.js
+++ firebase.js
@@ -5,13 +5,7 @@ import "@firebase/storage";
 
 // Your web app's Firebase configuration, which you copy-pasted from Step 6
 const firebaseConfig = {
-  apiKey: "AIzaSyC7CQwBSzjC_tlEiMd2Mc8Sh9Fb_Cwc1p8",
-  authDomain: "chapsnat-3f4f7.firebaseapp.com",
-  projectId: "chapsnat-3f4f7",
-  storageBucket: "chapsnat-3f4f7.appspot.com",
-  messagingSenderId: "239440555368",
-  appId: "1:239440555368:web:d7d431a3733e778d273add",
-  measurementId: "G-W4Y70B8JL2",
+  // CONFIG INFO GOES HERE
 };
 // Initialize Firebase
 
