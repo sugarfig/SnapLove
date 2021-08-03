c5c0a56c4531c3f870e05e0fc02109898d41a487
Ashley Manese <ashleymanese31@gmail.com>
2021-08-03 10:49:40 -0700

    edited icons to have gradient

--- screens/EditCategoryScreen.js
+++ screens/EditCategoryScreen.js
@@ -14,7 +14,10 @@ import { Dimensions } from "react-native";
 import CategoryCard from "../components/CategoryCard";
 
 
-const jobs  = require("../assets/jobs-1.5x.png");
+const jobs  = require("../assets/jobOutline.png");
+const counsling  = require("../assets/counslingOutline.png");
+const internship  = require("../assets/internshipsOutline.png");
+const workshop  = require("../assets/Workshop-1.5x.png");
 var height = Dimensions.get('window').height;
 function EditCategoryScreen(){
     return(
@@ -33,9 +36,9 @@ function EditCategoryScreen(){
           <View style={{display:'flex',alignItems:'center'}}>
           
             <CategoryCard img={jobs} name='Jobs'></CategoryCard>
-            <CategoryCard img={jobs} name='Jobs'></CategoryCard>
-            <CategoryCard img={jobs} name='Jobs'></CategoryCard>
-            <CategoryCard img={jobs} name='Jobs'></CategoryCard>
+            <CategoryCard img={counsling} name='Counseling'></CategoryCard>
+            <CategoryCard img={internship} name='Internship'></CategoryCard>
+            <CategoryCard img={workshop} name='Workshop'></CategoryCard>
 
 
           </View>
